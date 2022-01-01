26c08ac08b6262e5cf781735178557f961323da0
Vinishk0 <Vinishko01@yandex.ru>
2022-01-01 23:34:05 +0500

    Исправлены баги; сделанно отображение атаки и жизни карт

--- easybot.py
+++ easybot.py
@@ -17,16 +17,19 @@ class EasyBot():
         self.card = []
         return card, self.place, self.deck
 
-    def choice_card(self):
-        if self.deck and len(inventory_bot) < 6:
-            self.new_card = choice(self.deck)
-            del self.deck[self.deck.index(self.new_card)]
-            inventory_bot.append(self.new_card)
-        if inventory_bot:
-            self.card = choice(inventory_bot)
-            del inventory_bot[inventory_bot.index(self.card)]
+    def choice_card(self, delete=None):
+        if delete == None:
+            if self.deck and len(inventory_bot) < 6:
+                self.new_card = choice(self.deck)
+                del self.deck[self.deck.index(self.new_card)]
+                inventory_bot.append(self.new_card)
+            if inventory_bot and len(self.place_occupied) < 4:
+                self.card = choice(inventory_bot)
+                del inventory_bot[inventory_bot.index(self.card)]
+            else:
+                self.card = None
         else:
-            self.card = None
+            del self.place_occupied[delete]
 
     def choice_place(self):
         breakk = False
--- main.py
+++ main.py
@@ -8,7 +8,8 @@ bot_cards = []
 list = []
 bot_place_occupied = []
 my_place_occupied = []
-inventory_player = []
+inventory_player = [(7, 3, 'card_1_1'), (2, 6, 'card_1_2'), (3, 3, 'card_1_3')]
+
 
 class BaseGame:
     def __init__(self):
@@ -19,15 +20,17 @@ class BaseGame:
         self.inventory_card = pygame.sprite.Group()
         self.vil_x, self.vil_y, self.x_old, self.y_old, self.x_new, self.y_new = 1000, 550, 0, 0, 0, 0
         self.my_hp = 15
-        self.bot_hp =15
+        self.bot_hp = 15
         self.move = False
         self.movement = False
         self.vil = ()
         self.close = False
         self.ch = False
         self.deck = [(1, 1, 'card_1_1'), (1, 2, 'card_1_2'), (1, 3, 'card_1_3'), (1, 4, 'card_1_4'), (1, 5, 'card_1_5'),
-        (2, 1, 'card_2_1'), (2, 2, 'card_2_2'), (2, 3, 'card_2_3'), (2, 4, 'card_2_4'), (2, 5, 'card_2_5'),
-        (3, 1, 'card_3_1'), (3, 2, 'card_3_2'), (3, 3, 'card_3_3'), (3, 4, 'card_3_4'), (3, 5, 'card_3_5')]
+                     (1, 1, 'card_1_1'), (1, 2, 'card_1_2'), (1, 3, 'card_1_3'), (1, 4, 'card_1_4'), (1, 5, 'card_1_5')]
+        # self.deck = [(1, 1, 'card_1_1'), (1, 2, 'card_1_2'), (1, 3, 'card_1_3'), (1, 4, 'card_1_4'), (1, 5, 'card_1_5'),
+        # (2, 1, 'card_2_1'), (2, 2, 'card_2_2'), (2, 3, 'card_2_3'), (2, 4, 'card_2_4'), (2, 5, 'card_2_5'),
+        # (3, 1, 'card_3_1'), (3, 2, 'card_3_2'), (3, 3, 'card_3_3'), (3, 4, 'card_3_4'), (3, 5, 'card_3_5')]
         self.eb = EasyBot()
         self.updete_image()
 
@@ -58,6 +61,10 @@ class BaseGame:
             image = self.load_image(self.vil[2] + '.png')
             image1 = pygame.transform.scale(image, (150, 225))
             self.spr = self.screen.blit(image1, (1000, 550))
+            dm, hp, img = self.vil
+            font = pygame.font.Font(None, 30)
+            text = font.render(f"{dm}                 {hp}", True, (0, 0, 0))
+            self.screen.blit(text, (1015, 745))
             self.vil_x, self.vil_y = 1000, 550
         image = self.load_image('pole.png')
         image1 = pygame.transform.scale(image, (150, 225))
@@ -74,12 +81,24 @@ class BaseGame:
                 image1 = pygame.transform.scale(image, (150, 225))
                 if i[-1] == 0:
                     self.screen.blit(image1, (150, 450))
+                    font = pygame.font.Font(None, 30)
+                    text = font.render(f"{i[0]}                 {i[1]}", True, (0, 0, 0))
+                    self.screen.blit(text, (165, 645))
                 elif i[-1] == 1:
                     self.screen.blit(image1, (350, 450))
+                    font = pygame.font.Font(None, 30)
+                    text = font.render(f"{i[0]}                 {i[1]}", True, (0, 0, 0))
+                    self.screen.blit(text, (365, 645))
                 elif i[-1] == 2:
                     self.screen.blit(image1, (550, 450))
+                    font = pygame.font.Font(None, 30)
+                    text = font.render(f"{i[0]}                 {i[1]}", True, (0, 0, 0))
+                    self.screen.blit(text, (565, 645))
                 elif i[-1] == 3:
                     self.screen.blit(image1, (750, 450))
+                    font = pygame.font.Font(None, 30)
+                    text = font.render(f"{i[0]}                 {i[1]}", True, (0, 0, 0))
+                    self.screen.blit(text, (765, 645))
                 else:
                     del list[list.index(i)]
         if bot_cards:
@@ -89,15 +108,26 @@ class BaseGame:
                 image1 = pygame.transform.scale(image, (150, 225))
                 if i[-1] == 0:
                     self.screen.blit(image1, (150, 100))
+                    font = pygame.font.Font(None, 30)
+                    text = font.render(f"{i[0]}                 {i[1]}", True, (0, 0, 0))
+                    self.screen.blit(text, (165, 295))
                 elif i[-1] == 1:
                     self.screen.blit(image1, (350, 100))
+                    font = pygame.font.Font(None, 30)
+                    text = font.render(f"{i[0]}                 {i[1]}", True, (0, 0, 0))
+                    self.screen.blit(text, (365, 295))
                 elif i[-1] == 2:
                     self.screen.blit(image1, (550, 100))
+                    font = pygame.font.Font(None, 30)
+                    text = font.render(f"{i[0]}                 {i[1]}", True, (0, 0, 0))
+                    self.screen.blit(text, (565, 295))
                 elif i[-1] == 3:
                     self.screen.blit(image1, (750, 100))
+                    font = pygame.font.Font(None, 30)
+                    text = font.render(f"{i[0]}                 {i[1]}", True, (0, 0, 0))
+                    self.screen.blit(text, (765, 295))
                 else:
                     del list[list.index(i)]
-
 
         for i in range(len(inventory_player)):
             x = 400 + i * 30
@@ -110,16 +140,18 @@ class BaseGame:
         image = self.load_image('move.png')
         image1 = pygame.transform.scale(image, (100, 50))
         self.screen.blit(image1, (10, 360))
-
-        font = pygame.font.Font(None, 30)
-        text = font.render(f"{len(self.deck)}/35", True, (227, 37, 107))
-        self.screen.blit(text, (1000, 530))
         if None != cart:
             pass
         if self.deck:
             image = self.load_image('carta3.png')
             image1 = pygame.transform.scale(image, (200, 300))
             self.screen.blit(image1, (990, 200))
+            font = pygame.font.Font(None, 30)
+            text = font.render(f"{len(self.deck)}/35", True, (255, 255, 255))
+            self.screen.blit(text, (1100, 530))
+        font = pygame.font.Font(None, 30)
+        text = font.render(f"{self.my_hp}/{self.bot_hp}", True, (255, 255, 255))
+        self.screen.blit(text, (1100, 30))
         if self.movement:
             self.dragging()
 
@@ -153,6 +185,10 @@ class BaseGame:
             image = self.load_image(name_image + '.png')
             image1 = pygame.transform.scale(image, (150, 225))
             self.screen.blit(image1, (x, y))
+            dm, hp, img = inventory_player[i - 2]
+            font = pygame.font.Font(None, 30)
+            text = font.render(f"{dm}                 {hp}", True, (0, 0, 0))
+            self.screen.blit(text, (x + 15, y + 195))
 
     def click_card(self):
         index = None
@@ -189,6 +225,9 @@ class BaseGame:
             image = self.load_image(self.vil[2] + '.png')
             image1 = pygame.transform.scale(image, (150, 225))
             self.spr = self.screen.blit(image1, (self.vil_x, self.vil_y))
+            font = pygame.font.Font(None, 30)
+            text = font.render(f"{self.vil[0]}                 {self.vil[1]}", True, (0, 0, 0))
+            self.screen.blit(text, (self.vil_x + 15, self.vil_y + 195))
 
     def functions(self):
         mouse_pos = self.mouse_pos
@@ -227,8 +266,7 @@ class BaseGame:
             bot_cards.append((dm, hp, img, self.place_bot))
             bot_place_occupied.append(self.place_bot)
             self.deck = new_deck
-            self.attack('bot')
-
+        self.attack('bot')
 
     def attack(self, side):
         if side == 'bot':
@@ -237,7 +275,7 @@ class BaseGame:
                 if i in my_place_occupied:
                     try:
                         my_dm, my_hp, my_img, my_ind = my_cards[my_place_occupied.index(i)]
-                        if my_hp - bot_dm < 0:
+                        if my_hp - bot_dm <= 0:
                             del my_cards[my_place_occupied.index(i)]
                             del my_place_occupied[my_place_occupied.index(i)]
                         else:
@@ -252,7 +290,8 @@ class BaseGame:
                 if i in bot_place_occupied:
                     try:
                         bot_dm, bot_hp, bot_img, bot_ind = bot_cards[bot_place_occupied.index(i)]
-                        if bot_hp - my_dm < 0:
+                        if bot_hp - my_dm <= 0:
+                            self.eb.choice_card(bot_place_occupied.index(i))
                             del bot_cards[bot_place_occupied.index(i)]
                             del bot_place_occupied[bot_place_occupied.index(i)]
                         else:
@@ -261,12 +300,7 @@ class BaseGame:
                         self.bot_hp -= my_dm
                 else:
                     self.bot_hp -= my_dm
-        print('Твои жизни', self.my_hp)
-        print('Жизни бота', self.bot_hp)
         self.updete_image()
-
-
-
 
     def run(self, update=False):
         if update == False:
@@ -289,6 +323,7 @@ class BaseGame:
             pygame.display.flip()
             self.clock.tick(60)
         pygame.quit()
+
 
 base_game = BaseGame()
 base_game.run()
