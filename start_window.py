350d78b99c545a0ce75cf81e69a7e332215adc66
Я <sergo.sergej.05@yandex.ru>
2022-01-11 21:36:22 +0500

    Стартовое окно игры

--- start_window.py
+++ start_window.py
@@ -1,18 +1,25 @@
 import os
 import sys
 import pygame
+from choice_lvl import Levels
+
 
 class Start:
     def __init__(self):
         pygame.init()
-        self.size = self.WIDTH, self.HEIGHT = 800, 600
+        self.size = self.WIDTH, self.HEIGHT = 1200, 800
         self.screen = pygame.display.set_mode(self.size)
         self.clock = pygame.time.Clock()
         self.but_sound = pygame.mixer.Sound('data/but_sound.mp3')
-        pygame.display.set_caption('Меню')
-        self.music = pygame.mixer.Sound('data/start_mus.mp3')
         self.FPS = 60
+        self.sound_count = 1
+        self.start_music = 'data/start_mus.mp3'
+        pygame.mixer.music.load(self.start_music)
+        pygame.mixer.music.play(-1)
+        pygame.mixer.music.set_volume(0.1)
         self.start_screen()
+
+    '''Выбор изображения из папки проекта data'''
 
     def load_image(self, name, colorkey=None):
         fullname = os.path.join('data', name)
@@ -31,6 +38,8 @@ class Start:
 
         return image
 
+    '''Функционал и анимация нажатия кнопок'''
+
     def buttons(self, x, y, width, height, photo_name1, photo_name2, num):
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
@@ -42,12 +51,8 @@ class Start:
                     pygame.mixer.Sound.play(self.but_sound)
                     pygame.time.delay(300)
                     if num == 0:
-                        pass
-                    if num == 1:
-                        pass
+                        Levels()
                     if num == 2:
-                        pass
-                    if num == 3:
                         sys.exit()
             else:
                 fon = pygame.transform.scale(self.load_image(photo_name2), (width, height))
@@ -56,20 +61,45 @@ class Start:
             fon = pygame.transform.scale(self.load_image(photo_name2), (width, height))
             self.screen.blit(fon, (x, y))
 
-    def start_screen(self):
-        pygame.mixer.Sound.play(self.music)
+    '''Включение и выключения музыки'''
+
+    def sounds_point(self):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        if 1150 < mouse[0] < 1150 + 25:
+            if 30 < mouse[1] < 30 + 25:
+                if click[0] == 1:
+                    self.sound_count += 1
+                    if self.sound_count % 2 == 0:
+                        pygame.mixer.music.pause()
+                    else:
+                        pygame.mixer.music.unpause()
+
+    '''Перерисовка окна меню'''
+
+    def update_image(self):
         fon = pygame.transform.scale(self.load_image('start_back.jpg'), (self.WIDTH, self.HEIGHT))
         self.screen.blit(fon, (0, 0))
         fon = pygame.transform.scale(self.load_image('1.png'), (250, 100))
-        self.screen.blit(fon, (275, 30))
+        self.screen.blit(fon, (480, 30))
+        self.buttons(500, 165, 200, 80, 'play2.png', 'play1.png', 0)
+        self.buttons(500, 265, 200, 80, 'shop2.png', 'shop1.png', 1)
+        self.buttons(500, 365, 200, 80, 'rules_lvl2.png', 'rules_lvl.png', 2)
+        self.buttons(500, 465, 200, 80, 'exit1.png', 'exit2.png', 3)
+        if self.sound_count % 2 == 0:
+            sound_icon = pygame.transform.scale(self.load_image('sound2.png'), (25, 25))
+            self.screen.blit(sound_icon, (1150, 30))
+        else:
+            sound_icon = pygame.transform.scale(self.load_image('sound1.png'), (25, 25))
+            self.screen.blit(sound_icon, (1150, 30))
 
+    '''Основной цикл окна'''
+
+    def start_screen(self):
         while True:
             for event in pygame.event.get():
-                self.buttons(300, 165, 200, 80, 'play2.png', 'play1.png', 0)
-                self.buttons(300, 265, 200, 80, 'train2.png', 'train1.png', 1)
-                self.buttons(300, 365, 200, 80, 'shop2.png', 'shop1.png', 2)
-                self.buttons(300, 465, 200, 80, 'exit1.png', 'exit2.png', 3)
-
+                self.update_image()
+                self.sounds_point()
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
