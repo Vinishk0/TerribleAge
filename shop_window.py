b5fbc8c0262e6762c5580043052d26c59fbb9073
Я <sergo.sergej.05@yandex.ru>
2022-01-13 01:51:24 +0500

    Начало магазина

--- shop_window.py
+++ shop_window.py
@@ -6,12 +6,11 @@ import pygame
 class Shop:
     def __init__(self):
         pygame.init()
-        self.size = self.WIDTH, self.HEIGHT = 800, 600
+        self.size = self.WIDTH, self.HEIGHT = 1200, 800
         self.screen = pygame.display.set_mode(self.size)
         self.clock = pygame.time.Clock()
         self.but_sound = pygame.mixer.Sound('data/but_sound.mp3')
-        pygame.display.set_caption('Магазин')
-
+        self.sound_count = 1
         self.FPS = 60
         self.start_screen()
 
@@ -32,21 +31,60 @@ class Shop:
 
         return image
 
+    def buttons(self, x, y, width, height, photo_name1, photo_name2, num):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        if x < mouse[0] < x + width:
+            if y < mouse[1] < y + height:
+                fon = pygame.transform.scale(self.load_image(photo_name1), (width, height))
+                self.screen.blit(fon, (x, y))
+                if click[0] == 1:
+                    pygame.mixer.Sound.play(self.but_sound)
+                    pygame.time.delay(300)
+                    if num == 0:
+                        from start_window import Start
+                        Start()
+            else:
+                fon = pygame.transform.scale(self.load_image(photo_name2), (width, height))
+                self.screen.blit(fon, (x, y))
+        else:
+            fon = pygame.transform.scale(self.load_image(photo_name2), (width, height))
+            self.screen.blit(fon, (x, y))
+
+    def update_image(self):
+        fon = pygame.transform.scale(self.load_image('start_back.jpg'), (self.WIDTH, self.HEIGHT))
+        self.screen.blit(fon, (0, 0))
+        self.buttons(980, 700, 200, 80, 'menu_lvl2.png', 'menu_lvl.png', 0)
+        if self.sound_count % 2 == 0:
+            sound_icon = pygame.transform.scale(self.load_image('sound2.png'), (25, 25))
+            self.screen.blit(sound_icon, (1150, 30))
+        else:
+            sound_icon = pygame.transform.scale(self.load_image('sound1.png'), (25, 25))
+            self.screen.blit(sound_icon, (1150, 30))
+
+    def sounds_point(self):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        if 1150 < mouse[0] < 1150 + 25:
+            if 30 < mouse[1] < 30 + 25:
+                if click[0] == 1:
+                    self.sound_count += 1
+                    if self.sound_count % 2 == 0:
+                        pygame.mixer.music.pause()
+                    else:
+                        pygame.mixer.music.unpause()
+
     def start_screen(self):
         pygame.mixer.music.load('data/start_mus.mp3')
         pygame.mixer.music.play(-1)
         pygame.mixer.music.set_volume(0.1)
 
-        fon = pygame.transform.scale(self.load_image('start_back.jpg'), (self.WIDTH, self.HEIGHT))
-        self.screen.blit(fon, (0, 0))
-
         while True:
             for event in pygame.event.get():
+                self.update_image()
+                self.sounds_point()
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
             pygame.display.flip()
             self.clock.tick(self.FPS)
-
-
-Shop()
