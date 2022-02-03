903d5468eb7c09ce0c033ecc8525d630139881ff
Vinishk0 <94686362+Vinishk0@users.noreply.github.com>
2022-02-03 19:58:39 +0500

    Коментарии

---
+++ games_bots/easybot.py
@@ -0,0 +1,44 @@
+from random import choice
+
+
+class EasyBot():
+    # Легкий бот
+    def __init__(self):
+        self.place_occupied = []
+        self.deck = []
+        self.inventory_bot = []
+        self.bot_cards = []
+
+    def return_func(self, deck):
+        self.deck = deck
+        self.choice_card()
+        self.choice_place()
+        card = self.card
+        self.card = []
+        return card, self.place, self.deck
+
+    def choice_card(self, delete=None):
+        if delete == None:
+            if self.deck and len(self.inventory_bot) < 6:
+                self.new_card = choice(self.deck)
+                del self.deck[self.deck.index(self.new_card)]
+                self.inventory_bot.append(self.new_card)
+            if self.inventory_bot and len(self.place_occupied) < 4:
+                self.card = choice(self.inventory_bot)
+                del self.inventory_bot[self.inventory_bot.index(self.card)]
+            else:
+                self.card = None
+        else:
+            del self.place_occupied[delete]
+
+    def choice_place(self):
+        breakk = False
+        for i in range(4):
+            if i not in self.place_occupied:
+                self.place_occupied.append(i)
+                self.place = i
+                breakk = True
+                break
+        if not breakk:
+            self.place = None
+
---
+++ games_bots/normalbot.py
@@ -0,0 +1,56 @@
+from random import choice
+
+
+class NormalBot():
+    # бот нормального уровня сложности
+    def __init__(self):
+        self.place_occupied = []
+        self.deck = []
+        self.inventory_bot = []
+        self.bot_cards = []
+
+    def return_func(self, deck, new_laid_place, new_laid_card):
+        self.deck = deck
+        self.new_laid_place = new_laid_place
+        self.new_laid_card = new_laid_card
+        self.choice_card()
+        self.choice_place()
+        card = self.card
+        self.card = []
+        return card, self.place, self.deck
+
+    def choice_card(self, delete=None):
+        if delete == None:
+            if self.deck and len(self.inventory_bot) < 6:
+                self.new_card = choice(self.deck)
+                del self.deck[self.deck.index(self.new_card)]
+                self.inventory_bot.append(self.new_card)
+            if self.inventory_bot and len(self.place_occupied) < 4:
+                if self.new_laid_card[0] >= 3:
+                    self.inventory_bot.sort(key=lambda x: (x[1], x[0]))
+                    self.card = self.inventory_bot[-1]
+                elif self.new_laid_card[1] >= 3:
+                    self.inventory_bot.sort(key=lambda x: (x[0], x[1]))
+                    self.card = self.inventory_bot[-1]
+                else:
+                    self.card = choice(self.inventory_bot)
+                    del self.inventory_bot[self.inventory_bot.index(self.card)]
+            else:
+                self.card = None
+        else:
+            del self.place_occupied[delete]
+
+    def choice_place(self):
+        if self.new_laid_place not in self.place_occupied:
+            self.place = self.new_laid_place
+            self.place_occupied.append(self.place)
+        else:
+            breakk = False
+            for i in range(4):
+                if i not in self.place_occupied:
+                    self.place_occupied.append(i)
+                    self.place = i
+                    breakk = True
+                    break
+            if not breakk:
+                self.place = None
